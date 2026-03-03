f5eef1aa039ed8cb74e39100fc4aa07211929363
Rishit Srivastava <srivris55@gmail.com>
2026-03-03 23:23:15 +0530

    UC1: Print OOPS to Console

--- srk/OOPSBannerApp.java
+++ srk/OOPSBannerApp.java
@@ -1,5 +1,12 @@
 package srk;
-
+/**
+ * OOPS Banner App
+ *
+ * UC1: Print OOPS to Console
+ *
+ * @author Abhiram
+ * @version 1.0
+ */
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
